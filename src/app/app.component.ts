503617b0bebc3cf43149ec748de6c9babe071feb
Saiprava khilar <saipravakhilar88@gmail.com>
2023-12-24 22:35:36 +0530

    Angular1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'myCal';
+  toshow = '0';
+  currvalue = "";
+
+  writeToInput(value:string){ 
+    this.currvalue = this.currvalue + value;
+    this.toshow = this.currvalue;
+  }
+  equals(){
+    this.toshow = eval(this.currvalue);
+    this.currvalue = this.toshow;
+  }
+  clear(){
+    this.toshow = "0";
+    this.currvalue = "";
+  }
+  back(){
+    this.currvalue = this.currvalue.slice(0,-1);
+    this.toshow = this.currvalue;
+    if (this.toshow == '') {this.toshow = '0'} 
+  }
+  calcValue(solve:any){
+    if (solve.charAt(0) == '0') {
+      solve = solve.slice(1,)
+    }
+    this.toshow = eval(solve); 
+  }
 }
